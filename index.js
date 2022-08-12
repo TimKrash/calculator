1dc18c1fb4da99511b4c2bc49685fd7f5ce5758e
Tim Krashevsky <tim.krashevsky@gmail.com>
2022-08-12 13:47:57 -0500

    added logic for clear and delete

--- index.js
+++ index.js
@@ -16,6 +16,10 @@ let displayScreen = document.querySelector('.display');
 let clearButton = document.querySelector('.clear');
 let deleteButton = document.querySelector('.delete');
 
+    // Select display top element
+let displayTop = document.querySelector('.text.top');
+let displayBottom = document.querySelector('.text.bottom');
+
 
 const add = (a, b) => parseInt(a) + parseInt(b);
 const subtract = (a, b) => parseInt(a) - parseInt(b);
@@ -52,15 +56,10 @@ const display = (e) => {
         displayValue += e.target.textContent;
     }
 
-    let displayScreenValue = displayScreen.querySelector('.text.bottom');
-    displayScreenValue.textContent = displayValue;
+    displayBottom.textContent = displayValue;
 }
 
 const queueOperation = (e) => {
-    // Select display top element
-    let displayTop = document.querySelector('.text.top');
-    let displayBottom = document.querySelector('.text.bottom');
-
     // Push existing display value to queue and then set to null
     if (displayValue) {
         displayQueue.push(displayValue);
@@ -83,6 +82,7 @@ const queueOperation = (e) => {
             break;
         case "=":
             operator = Operators.Equals;
+            displayTop.textContent = displayQueue.join(' ');
             break;
     }
 
@@ -93,15 +93,47 @@ const queueOperation = (e) => {
     }
 
     if (displayQueue.length == 4) {
-        let slicedArr = displayQueue.slice(0, 3);
-        let answer = operate(slicedArr[1], slicedArr[0], slicedArr[2]);
-        displayQueue.splice(0, 3, answer.toString());
-    }
-    displayBottom.textContent = displayQueue[0];
-    displayTop.textContent = displayQueue.join(' ');
+        let initOperator = displayQueue[1];
+        let nextOperator = displayQueue[3];
+        let numOne = displayQueue[0];
+        let numTwo = displayQueue[2];
 
-    displayTop.textContent = displayTop.textContent.replace("*", "&times;");
-    displayTop.textContent = displayTop.textContent.replace("/", "&#247;");
+        let answer = operate(initOperator, numOne, numTwo);
+        if (nextOperator === Operators.Equals) {
+            displayTop.textContent = displayQueue.join(' ');
+            displayBottom.textContent = answer;
+            displayQueue.splice(0, 4, answer.toString());
+        } else {
+            console.log(displayQueue);
+            displayQueue.splice(0, 3, answer.toString());
+            displayTop.textContent = displayQueue.join(' ');
+            displayBottom.textContent = answer;
+        }
+    } else {
+        displayTop.textContent = displayQueue.join(' ');
+        displayBottom.textContent = displayQueue[0];
+    }
+
+}
+
+// Clear and delete logic
+const clearCalculator = (e) => {
+   displayTop.textContent = null;
+   displayBottom.textContent = 0;
+   displayQueue = []; 
+}
+
+const deleteEntry = (e) => {
+    if (displayValue === "0" || !displayValue) {
+        displayValue === "0";
+        return;
+    }
+
+    displayValue = displayValue.slice(0, -1);
+    if (displayValue.length == 0) {
+        displayValue = "0";
+    }
+    displayBottom.textContent = displayValue;
 }
 
 // Button event listeners
@@ -117,3 +149,6 @@ let operatorButtons = calcButtons.filter(button => {
 
 numberButtons.forEach(button => button.addEventListener('click', display));
 operatorButtons.forEach(button => button.addEventListener('click', queueOperation));
+
+clearButton.addEventListener('click', clearCalculator);
+deleteButton.addEventListener('click', deleteEntry);
