d96056ef998c285654296e42124d439dd1a5cc65
Tim Krashevsky <tim.krashevsky@gmail.com>
2022-08-12 12:12:42 -0500

    prelim logic working

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
+// Global variables
 const Operators = {
     Add: "+",
     Subtract: "-",
-    Multiply: "*",
-    Divide: "/" 
+    Multiply: "×",
+    Divide: "÷",
+    Equals: "=" 
 }
 
-const add = (a, b) => a + b;
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
+let displayQueue = [];
+let displayValue = "0";
+
+// Button event listeners
+let calcButtons = [...document.querySelectorAll('.button')]; 
+let displayScreen = document.querySelector('.display');
+let clearButton = document.querySelector('.clear');
+let deleteButton = document.querySelector('.delete');
+
+
+const add = (a, b) => parseInt(a) + parseInt(b);
+const subtract = (a, b) => parseInt(a) - parseInt(b);
+const multiply = (a, b) => parseInt(a) * parseInt(b);
+const divide = (a, b) => parseInt(a) / parseInt(b);
 
 /**
  * 
@@ -29,4 +41,79 @@ const operate = (operator, numOne, numTwo) => {
     }
 }
 
-console.log(operate("/", 5, 6));
+/**
+ * 
+ * @param {Event} e 
+ */
+const display = (e) => {
+    if (displayValue === "0" || !displayValue) {
+        displayValue = e.target.textContent;
+    } else {
+        displayValue += e.target.textContent;
+    }
+
+    let displayScreenValue = displayScreen.querySelector('.text.bottom');
+    displayScreenValue.textContent = displayValue;
+}
+
+const queueOperation = (e) => {
+    // Select display top element
+    let displayTop = document.querySelector('.text.top');
+    let displayBottom = document.querySelector('.text.bottom');
+
+    // Push existing display value to queue and then set to null
+    if (displayValue) {
+        displayQueue.push(displayValue);
+        displayValue = null;
+    }
+
+    let operator = e.target.textContent;
+    switch (operator) {
+        case "+":
+            operator = Operators.Add;
+            break;
+        case "-":
+            operator = Operators.Subtract;
+            break;
+        case "&times;":
+            operator = Operators.Multiply;
+            break;
+        case "&#247;":
+            operator = Operators.Divide;
+            break;
+        case "=":
+            operator = Operators.Equals;
+            break;
+    }
+
+    if (displayQueue.length == 2) {
+        displayQueue[1] = operator
+    } else {
+        displayQueue.push(operator);
+    }
+
+    if (displayQueue.length == 4) {
+        let slicedArr = displayQueue.slice(0, 3);
+        let answer = operate(slicedArr[1], slicedArr[0], slicedArr[2]);
+        displayQueue.splice(0, 3, answer.toString());
+    }
+    displayBottom.textContent = displayQueue[0];
+    displayTop.textContent = displayQueue.join(' ');
+
+    displayTop.textContent = displayTop.textContent.replace("*", "&times;");
+    displayTop.textContent = displayTop.textContent.replace("/", "&#247;");
+}
+
+// Button event listeners
+let numberButtons = calcButtons.filter(button => {
+    return button.className.toLowerCase().indexOf("multiply") === -1 &&
+    button.className.toLowerCase().indexOf("divide") === -1 && button.className.toLowerCase().indexOf("subtract") === -1 &&
+    button.className.toLowerCase().indexOf("add") === -1 && button.className.toLowerCase().indexOf("equals") === -1;
+})
+let operatorButtons = calcButtons.filter(button => {
+    return button.className.includes("multiply") || button.className.includes("divide") ||
+    button.className.includes("equals") || button.className.includes("subtract") || button.className.includes("add");
+})
+
+numberButtons.forEach(button => button.addEventListener('click', display));
+operatorButtons.forEach(button => button.addEventListener('click', queueOperation));
